099eb2dd5e40a1cd7c8992db32173ba4398db885
hyeri <parkri1240@gmail.com>
2022-11-18 15:44:37 +0900

    스크립트 추가

--- js/script.js
+++ js/script.js
@@ -18,8 +18,33 @@ $(document).ready(function(){
   });
 });
 
-//화면전환시 애니메이션 실행
+//휠 이벤트
+window.addEventListener("wheel", function(e){
+	e.preventDefault();
+},{passive : false});
 
+var $html = $("html");
+var page = 1;
+var lastPage = $(".content").length;
+ 
+$html.animate({scrollTop:0},100);
+
+$(window).on("wheel", function(e){
+	if($html.is(":animated")) return;
+	if(e.originalEvent.deltaY > 0){
+		if(page== lastPage) return;
+		page++;
+	}else if(e.originalEvent.deltaY < 0){
+		if(page == 1) return;
+		page--;
+	}
+	var posTop = (page-1) * $(window).height();
+ 
+	$html.animate({scrollTop : posTop});
+});
+
+//화면전환시 애니메이션 실행
+  AOS.init();
 
 //메뉴 클릭
 $('.menu-wrap').each(function(){
